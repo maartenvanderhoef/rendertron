0f3dda9f558f4e05eae76825075ca6d5ffc22601
Maarten van der Hoef <maarten@vanderhoef.eu>
2019-05-13 16:02:50 +0200

    Update rendertron.ts

--- src/rendertron.ts
+++ src/rendertron.ts
@@ -28,7 +28,7 @@ export class Rendertron {
     this.port = this.port || this.config.port;
 
 
-    const browser = await puppeteer.launch({ args: ['--no-sandbox'] });
+    const browser = await puppeteer.launch({ args: ['--no-sandbox','--disable-dev-shm-usage'] });
     this.renderer = new Renderer(browser, this.config);
 
     this.app.use(koaLogger());
